f7926377defc7a85d0d4ace6f491f70548ea44c9
KalinkinaMaria <mashyfycya@gmail.com>
2014-11-27 15:43:48 +0300

    Commit 10

--- scanstring.php
+++ scanstring.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with CorrectWriting.  If not, see <http://www.gnu.org/licenses/>.
-
+//Commit 10
 
 /**
  * Defines a scanning string  options
